b1d4c8e4549c82a1a14e1a8790ee8803153cb16d
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-09-23 20:41:39 +0500

    completed tasks

--- Activities/Activity2.js
+++ Activities/Activity2.js
@@ -1,9 +1,40 @@
 /**
  * Task 1
  */
-function sandwich (bread){
-        sandwich= bread/2;
-        console.log("Possible Sandwiches " +sandwich)
+
+function sandwich(bread, cheese) {
+    let total;
+    if (bread / 2 === cheese) {
+        return total = cheese;
     }
-    sandwich(4);
-    sandwich(8);
+    else if (cheese === 1) {
+        return total = 1;
+    }
+}
+console.log(sandwich(4, 2));
+
+var student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12
+};
+
+
+/**
+ * Task 2
+ * Program to list properties of a javascript object 
+ */
+console.log(Object.keys(student));
+
+
+/**
+ * Task 3
+ * 
+ */
+function deleteProperty(obj, key) {
+    console.log(student);
+    delete obj[key];
+    return obj;
+}
+let deleteRollNo = deleteProperty(student, "rollno");
+console.log(deleteRollNo);
