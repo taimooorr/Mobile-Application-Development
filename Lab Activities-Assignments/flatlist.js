eedf20868889175f33c1906a4a7522026f14ab5c
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-10-19 23:30:28 +0500

    updated flat list

--- Lab Activities-Assignments/flatlist.js
+++ Lab Activities-Assignments/flatlist.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { FlatList } from 'react-native-web';
+import { Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native';
+/**
+ * show data of 10b students in flat list react native
+ */
 export default function StudentRecord() {
-  const flatlistData = [{
-    id: '1',
-    name: 'John',
-    age: 20,
-    image: require('../assets/icon.png')
-  },
-  {
-    id: '2',
-    name: 'John',
-    age: 20,
-    image: require('../assets/icon.png')
-  }]
+  const records = [
+    { name: 'Ali', age: 20, id: '1' },
+    { name: 'Ahmed', age: 21, id: '2' },
+    { name: 'Hassan', age: 22, id: '3' },
+    { name: 'Kamran', age: 23, id: '4' },
+    { name: 'Kashif', age: 24, id: '5' },
+    { name: 'Kamran', age: 25, id: '6' },
+    { name: 'Kashif', age: 26, id: '7' },
+    { name: 'Kamran', age: 27, id: '8' },
+    { name: 'Kashif', age: 28, id: '9' },
+    { name: 'Kamran', age: 29, id: '10' },
+
+  ]
   return (
     <FlatList
-      data={flatlistData}
-      renderItem={(element, index) => {
+      data={records}
+      renderItem={({ item }) => {
         return (
-          <Image source={element.data.image} key={index.data.id}>
-            <Text>{element.data.name}</Text>
-            <Text>{element.data.age}</Text>
-          </Image>
+          <Text style={styles.textStyle}>{item.name} - {item.age}</Text>
         )
-      }
-
-
-      }
+      }}
     />
-
-
-  );
+  )
 };
 const styles = StyleSheet.create({
   textStyle: {
